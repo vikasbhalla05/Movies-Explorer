ca3cc095e75b6c0cbebe5289c9e04c7852a2c75e
Vikas Bhalla <vikas@ellingsentechnologies.com>
2024-11-14 18:46:55 +0530

    react router setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  }
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
